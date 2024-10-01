2bd7be8aff6905d47f4bc7c99df5d5949c0435c0
DELL <niheleeroui124@gmail.com>
2024-10-01 16:12:20 +0200

    commit1

---
+++ gestionconge/src/main/java/tn/esprit/gestionconge/GestioncongeApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionconge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncongeApplication.class, args);
+	}
+
+}
---
+++ gestionconge/src/test/java/tn/esprit/gestionconge/GestioncongeApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionconge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
