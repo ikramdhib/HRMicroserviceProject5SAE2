fb6b97fc511148230b2cf18ee8e5fceabc65ff05
DELL <niheleeroui124@gmail.com>
2024-10-03 14:14:30 +0200

    commit2

---
+++ APIGateway/src/main/java/tn/esprit/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package tn.esprit.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("gestionconge",r->r.path("/conges")
+						.uri("http://localhost:8087"))
+				.build();
+	}
+
+}
