aa50aad608369a5c17f6842660c5a8d38392034d
DELL <niheleeroui124@gmail.com>
2024-10-17 12:04:22 +0200

    commit2

--- APIGateway/src/main/java/tn/esprit/apigateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/tn/esprit/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,9 +20,23 @@ public class ApiGatewayApplication {
 	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
 		return builder.routes()
-				.route("gestionconge",r->r.path("/conges")
+				.route("gestionconge",r->r.path("/conges/**")
 						.uri("http://localhost:8087"))
 				.build();
+	}
+
+	@Bean
+	public CorsWebFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://localhost:4200"); // Ajouter les domaines autorisés ici
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
 	}
 
 }
